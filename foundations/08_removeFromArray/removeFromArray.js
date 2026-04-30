c525204cf7ebf4e493110bbcc9927fea946f4878
Kiyomi Murray <244055282+kiymur@users.noreply.github.com>
2026-04-30 18:25:50 -0400

    Create removeFromArray function
    * removeFromArray function takes two arguments; an array and an item.
    The function returns a new array that has the target item removed

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== item) {
+            newArray = newArray.concat(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
