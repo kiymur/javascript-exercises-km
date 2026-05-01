8514c82b55364c768d28141444ba536b0f1ee62b
Kiyomi Murray <244055282+kiymur@users.noreply.github.com>
2026-04-30 20:55:50 -0400

    Create the sumAll function
    * sumAll function takes two positive integers as arguments and returns
    the sum of every integer between, and including, them

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let arr = [];
 
+    for (let i = start; i <= end; i++) {
+        arr.unshift(i);
+    }
+
+    const productOfArray = arr.reduce((total, currentItem) => {
+        return total += currentItem;
+    }, 0);
+
+    return productOfArray;
 };
 
 // Do not edit below this line
