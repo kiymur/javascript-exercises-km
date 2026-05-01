514999b2c9fccde0406826004c51ac3a266262d4
Kiyomi Murray <244055282+kiymur@users.noreply.github.com>
2026-04-30 22:41:45 -0400

    Fix leapYears function for century years
    * added condition of not being divisible by 400 to line 3

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
     if (year % 4 === 0 && year % 100 !== 0) return true;
-    if (year % 4 === 0 && year % 100 === 0) return false;
+    if (year % 4 === 0 && year % 100 === 0 && year & 400 !== 0) return false;
     if (year % 400 === 0) return true;
     else return false;
 };
